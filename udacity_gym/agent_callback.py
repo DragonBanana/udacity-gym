a7b9ff5ec73f2d94db2aaedf314c4ddf0ad77f28
Davide Yi Xian Hu <davideyi.hu@polimi.it>
2024-06-19 14:43:40 +0200

    feat: added optional arguments into transformation callback

--- udacity_gym/agent_callback.py
+++ udacity_gym/agent_callback.py
@@ -106,6 +106,7 @@ class TransformObservationCallback(AgentCallback):
 
     def __call__(self, observation: UdacityObservation, *args, **kwargs):
         super().__call__(observation, *args, **kwargs)
-        augmented_image: torch.Tensor = self.transformation(observation.input_image, *args, **kwargs)
+        augmented_image: torch.Tensor = self.transformation(observation.input_image,
+                                                            mask=observation.semantic_segmentation, *args, **kwargs)
         image = torchvision.transforms.ToPILImage()(augmented_image.float())
         observation.input_image = np.asarray(image)
